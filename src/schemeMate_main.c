b4370f39e299a978ffd3e405ef1b3d3a53e2623c
Stefan Seibert <stefan.seibert@autodesk.com>
2017-04-08 23:24:17 +0200

    Using sm for schemeMate now as prefix

--- src/schemeMate_main.c
+++ src/schemeMate_main.c
@@ -1,20 +1,37 @@
 #include "schemeMate_main.h"
 
-#define stdInputStream
+static void initializeSystem() {
+    INPUT = new_fileStream(stdin);
+    SM_NIL = new_nil();
+    SM_TRUE = new_true();
+    SM_FALSE = new_false();
+    SM_VOID = new_void();
+    SM_EOF = new_eof();
+}
 
 static void repl()
 {
-	fprintf(stdout, ">");
-	SCM_OBJ expr = SCM_read(stdInputStream);
-	scm_print(expr);
+	sm_obj expr;
+	sm_obj result;
+
+	while (true) {
+		fprintf(stdout, ">>> ");
+		expr = sm_read(stdInputStream);
+		result = sm_eval(expr);
+		if (tag(result) != TAG_VOID) {
+	    	sm_print(stdout, result, PRINT_WRITE);
+	    	fprintf(stdout, "\n");
+		}
+	}
 }
 
 
 int main(int argc, char *argv[])
 {
 	init_system();
-	printf("Welcome to our (incomplete) Scheme\n");
-	scm_selftest();
+	printf("Welcome to schemeMate [0.1 ]\n");
+	selftest();
+	repl();
 	deinit_system();
 	exit(0);
 }
--- src/schemeMate_main.h
+++ src/schemeMate_main.h
@@ -1,21 +1,26 @@
-#pragma once
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
 #include "schemeMate_objects.h"
 #include "schemeMate_selftest.h"
-#include <stdlib.h>
 
+#ifdef DEBUG
+# define DBG_PRINT(x) fprintf x
+#else
+# define DBG_PRINT(x) /* as nothing */
+#endif
+
+sm_obj SM_NIL = NULL;
+sm_obj SM_TRUE = NULL;
+sm_obj SM_FALSE = NULL;
+sm_obj SM_VOID = NULL;
+sm_obj SM_EOF = NULL;
+sm_obj SM_QUOTE = NULL;
+static sm_stream INPUT = NULL;
+
+static void init_system();
+static void repl();
 int main(int argc, char *argv[]);
 
 
-/* macro functions to check 
-
-static inline BOOL isCons(SCM_OBJ obj)
-{
-	return hasTag(obj, TAG_CONS);
-}
-
-static inline SCM_OBJ car(SCM_OBJ obj)
-{
-	return hasTag(obj, TAG_CONS);
-}
-
-*/
+#endif // MAIN_HEADER
